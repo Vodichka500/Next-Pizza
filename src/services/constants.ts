6d94c5c1027b8375626354244f0647685a239281
Uladzislau Kamisarau <u.kamisarau@gmail.com>
2025-02-24 20:28:37 +0100

    changed static localhost variable

--- src/services/constants.ts
+++ src/services/constants.ts
@@ -1,4 +1,4 @@
-const BASE_URL='http://localhost:3000/api'
+const BASE_URL='/api'
 const INGRIDIENTS_PATH='/ingridients'
 const USERS_PATH='/users'
 const PRODUCTS_SEARCH_NAME_PATH='/products/searchName'
